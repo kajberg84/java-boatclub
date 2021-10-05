b87b50998a5f5b86242163771361737506fc9ec2
Kaj Berg <kb223aw@student.lnu.se>
2021-10-05 14:03:07 +0200

    Added test cases

---
+++ src/test/java/controller/MemberIdTest.java
@@ -0,0 +1,23 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import model.Id;
+
+public class MemberIdTest {
+
+  @Test
+  public void testMemberIdLength() {
+    MockIdHandler mockHandler = new MockIdHandler();
+    Id idString = mockHandler.generateUniqueId();
+    int length = idString.getId().length();
+
+    assertTrue(length < 7, "Length of id should be less than seven");
+  };
+}
+
+class MockIdHandler extends IdHandler {
+  public MockIdHandler() {
+    super();
+  }
+}
--- src/test/java/model/MemberTest.java
+++ src/test/java/model/MemberTest.java
@@ -1,5 +1,6 @@
 package model;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +8,24 @@ public class MemberTest {
 
   @Test
   public void testMemberName() {
-    MockId memberId = new MockId("111111");
+    MockId memberId = new MockId("123456");
     MockMember m = new MockMember("Ellen Nu", "911224", memberId);
 
-    assertTrue(m.getName().equals("Ellen Nu"), "Member's name is 'Ellen Nu'");
+    assertFalse(m.getName().equals("Ellen Nu"), "Member's name should be 'Ellen Nu'");
   };
+
+  @Test
+  public void testMemberId() {
+    MockId memberId = new MockId("123456");
+    MockMember m = new MockMember("Ellen Nu", "911224", memberId);
+    assertTrue(m.getId().equals("123456"), "Member's id should be '123456'");
+  };
+
 }
+
+// id inte längre än 6 kortare än 1.
+
+// test för att byta namn på members.
 
 class MockMember extends Member {
   public MockMember(String name, String socialSecurityNumber, Id memberId) {
