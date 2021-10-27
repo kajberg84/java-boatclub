d7d38993e8d09426199f499cf01f5c0cb3a5b322
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-10-27 09:49:43 +0200

    Move dependencies and functionality to menu classes

--- src/main/java/controller/BoatClubHandler.java
+++ src/main/java/controller/BoatClubHandler.java
@@ -1,168 +1,143 @@
 package controller;
 
-import java.util.Scanner;
-import model.Member;
 import model.MemberRegistry;
 import model.PersistentData;
-import view.Action;
-import view.BoatAction;
-import view.BoatView;
-import view.LoginView;
-import view.MemberAction;
-import view.MemberView;
 
 /**
  * Responsible for main operations in the boat club application.
  */
 public class BoatClubHandler {
-  private Scanner scan = new Scanner(System.in, "UTF-8");
-  private MemberView memberUi = new MemberView(scan);
   private MemberRegistry registry = new MemberRegistry();
-  private MemberHandler memberHandler = new MemberHandler(memberUi, registry);
-  private BoatView boatUi = new BoatView(scan);
-  private BoatHandler boatHandler = new BoatHandler(boatUi);
   private PersistentData persistentData = new PersistentData(registry);
-  private LoginView loginView = new LoginView(scan);
+  private Menu menu;
+  private Authenticator authenticator;
 
-  public BoatClubHandler() {
+  public BoatClubHandler(Authenticator auth) {
+    authenticator = auth;
   }
 
   public void start() {
-    if (login()) {
-      persistentData.load();
-      showMainMenu();
+    persistentData.load();
+    if (authenticator.login()) {
+      menu = new AuthenticatedMenu(registry);
+      menu.showMainMenu();
     } else {
-      loginFailed();
+      menu = new UnauthenticatedMenu(registry);
+      menu.showMainMenu();
     }
   }
 
-  private boolean login() {
-    String userName = loginView.promptForUserName();
-    String password = loginView.promptForPassword();
-    return validateCredentials(userName, password);
-  }
+  // private void showMainMenu() {
+  //   Action action = boatUi.promptForMainAction();
+  //   showSubMenu(action);
+  // }
 
-  private boolean validateCredentials(String userName, String password) {
-    String correctUserName = "santa";
-    String correctPassword = "rudolph";
-    return (userName.equals(correctUserName) && password.equals(correctPassword));
-  }
+  // private void showSubMenu(Action action) {
+  //   switch (action) {
+  //     case MEMBERS:
+  //       MemberAction memberAction = memberUi.promptForMemberAction();
+  //       handleMemberActions(memberAction);
+  //       break;
+  //     case BOATS:
+  //       BoatAction boatAction = boatUi.promptForBoatAction();
+  //       handleBoatAction(boatAction);
+  //       break;
+  //     case EXIT:
+  //       scan.close();
+  //       System.out.println("Goodbye!");
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
 
-  private void loginFailed() {
-    loginView.printLoginFailed();
-  }
+  // private void handleBoatAction(BoatAction action) {
+  //   switch (action) {
+  //     case ADD:
+  //       handleAddBoat();
+  //       showSubMenu(Action.BOATS);
+  //       break;
+  //     case EDIT:
+  //       handleEditBoat();
+  //       showSubMenu(Action.BOATS);
+  //       break;
+  //     case DELETE:
+  //       handleDeleteBoat();
+  //       showSubMenu(Action.BOATS);
+  //       break;
+  //     case BACK:
+  //       showMainMenu();
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
 
-  private void showMainMenu() {
-    Action action = boatUi.promptForMainAction();
-    showSubMenu(action);
-  }
+  // private void handleAddBoat() {
+  //   boatUi.printRegisterBoatHeader();
+  //   Member member = memberHandler.askForValidMember();
+  //   boatHandler.addNewBoat(member);
+  // }
 
-  private void showSubMenu(Action action) {
-    switch (action) {
-      case MEMBERS:
-        MemberAction memberAction = memberUi.promptForMemberAction();
-        handleMemberActions(memberAction);
-        break;
-      case BOATS:
-        BoatAction boatAction = boatUi.promptForBoatAction();
-        handleBoatAction(boatAction);
-        break;
-      case EXIT:
-        scan.close();
-        System.out.println("Goodbye!");
-        break;
-      default:
-        break;
-    }
-  }
+  // private void handleEditBoat() {
+  //   boatUi.printEditBoatHeader();
+  //   Member member = memberHandler.askForValidMember();
+  //   boatHandler.editBoat(member);
+  // }
 
-  private void handleBoatAction(BoatAction action) {
-    switch (action) {
-      case ADD:
-        handleAddBoat();
-        showSubMenu(Action.BOATS);
-        break;
-      case EDIT:
-        handleEditBoat();
-        showSubMenu(Action.BOATS);
-        break;
-      case DELETE:
-        handleDeleteBoat();
-        showSubMenu(Action.BOATS);
-        break;
-      case BACK:
-        showMainMenu();
-        break;
-      default:
-        break;
-    }
-  }
+  // private void handleDeleteBoat() {
+  //   boatUi.printDeleteBoatHeader();
+  //   Member member = memberHandler.askForValidMember();
+  //   boatHandler.deleteBoat(member);
+  // }
 
-  private void handleAddBoat() {
-    boatUi.printRegisterBoatHeader();
-    Member member = memberHandler.askForValidMember();
-    boatHandler.addNewBoat(member);
-  }
+  // private void handleMemberActions(MemberAction action) {
+  //   switch (action) {
+  //     case ADD:
+  //       handleAddMember();
+  //       showSubMenu(Action.MEMBERS);
+  //       break;
+  //     case EDIT:
+  //       handleEditMember();
+  //       showSubMenu(Action.MEMBERS);
+  //       break;
+  //     case VIEWALL:
+  //       handleViewAllMembers();
+  //       showSubMenu(Action.MEMBERS);
+  //       break;
+  //     case VIEWONE:
+  //       handleViewMember();
+  //       showSubMenu(Action.MEMBERS);
+  //       break;
+  //     case DELETE:
+  //       handleDeleteMember();
+  //       showSubMenu(Action.MEMBERS);
+  //       break;
+  //     case BACK:
+  //       showMainMenu();
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
 
-  private void handleEditBoat() {
-    boatUi.printEditBoatHeader();
-    Member member = memberHandler.askForValidMember();
-    boatHandler.editBoat(member);
-  }
-
-  private void handleDeleteBoat() {
-    boatUi.printDeleteBoatHeader();
-    Member member = memberHandler.askForValidMember();
-    boatHandler.deleteBoat(member);
-  }
-
-  private void handleMemberActions(MemberAction action) {
-    switch (action) {
-      case ADD:
-        handleAddMember();
-        showSubMenu(Action.MEMBERS);
-        break;
-      case EDIT:
-        handleEditMember();
-        showSubMenu(Action.MEMBERS);
-        break;
-      case VIEWALL:
-        handleViewAllMembers();
-        showSubMenu(Action.MEMBERS);
-        break;
-      case VIEWONE:
-        handleViewMember();
-        showSubMenu(Action.MEMBERS);
-        break;
-      case DELETE:
-        handleDeleteMember();
-        showSubMenu(Action.MEMBERS);
-        break;
-      case BACK:
-        showMainMenu();
-        break;
-      default:
-        break;
-    }
-  }
-
-  private void handleAddMember() {
-    memberHandler.registerMember();
-  }
+  // private void handleAddMember() {
+  //   memberHandler.registerMember();
+  // }
   
-  private void handleEditMember() {
-    memberHandler.editMember();
-  }
+  // private void handleEditMember() {
+  //   memberHandler.editMember();
+  // }
 
-  private void handleViewMember() {
-    memberHandler.viewMember();
-  }
+  // private void handleViewMember() {
+  //   memberHandler.viewMember();
+  // }
   
-  private void handleViewAllMembers() {
-    memberHandler.viewAllMembers();
-  }
+  // private void handleViewAllMembers() {
+  //   memberHandler.viewAllMembers();
+  // }
 
-  private void handleDeleteMember() {
-    memberHandler.deleteMember();
-  }
+  // private void handleDeleteMember() {
+  //   memberHandler.deleteMember();
+  // }
 }
