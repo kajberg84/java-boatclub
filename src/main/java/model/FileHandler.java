4522571427e0b2c8698461fcd6e249a313d0b2a5
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-10-28 18:21:29 +0200

    Add file handler

--- src/main/java/model/FileHandler.java
+++ src/main/java/model/FileHandler.java
@@ -1,17 +1,51 @@
 package model;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Responsible for loading and saving data.
+ */
 public class FileHandler implements Persistence {
+
+  private MemberRegistry registry;
+
+  public FileHandler(MemberRegistry registry) {
+    this.registry = registry;
+  }
 
   @Override
   public void load() {
-    // TODO Auto-generated method stub
-    
+    FileReader file;
+    String currentDir = System.getProperty("user.dir");
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+      file = new FileReader(currentDir + "/files/data.json");
+      ArrayList<Member> members = mapper.readValue(file, new TypeReference<ArrayList<Member>>(){});
+      for(Member m : members) {
+        registry.addMemberToRegistry(m);
+      }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
   }
 
   @Override
   public void save() {
-    // TODO Auto-generated method stub
-    
+    ArrayList<Member> members = registry.getAllMembers();
+    ObjectMapper mapper = new ObjectMapper();
+    String currentDir = System.getProperty("user.dir");
+    try {
+      FileWriter file = new FileWriter(currentDir + "/files/data.json");
+      mapper.writerWithDefaultPrettyPrinter().writeValue(file, members);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
-  
 }
